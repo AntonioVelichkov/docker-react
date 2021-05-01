7cb6d4ca366043a97bc046d6eac53b936a63dc1f
AntonioVelichkov <antonio.velichkov@gmail.com>
2021-05-01 10:02:28 +0300

    changed App.js file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Cool yee!
+          Testing feature/1 branch!
         </p>
         <a
           className="App-link"
